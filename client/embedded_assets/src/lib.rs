b0546815ddf788996334f8ea0661499c83961cf5
Voidentente <flood.void@gmail.com>
2022-12-25 18:24:36 +0100

    Remove embedded asset lifetime annotation

--- client/embedded_assets/src/lib.rs
+++ client/embedded_assets/src/lib.rs
@@ -1,11 +1,11 @@
-pub const ICON: &'static [u8] = include_bytes!("../assets/icon.qoi");
+pub const ICON: &[u8] = include_bytes!("../assets/icon.qoi");
 
-pub const FONT_DREAMS: &'static [u8] = include_bytes!("../assets/fonts/DadasDreams.ttf");
+pub const FONT_DREAMS: &[u8] = include_bytes!("../assets/fonts/DadasDreams.ttf");
 
-pub const FONT_SQUARE: &'static [u8] = include_bytes!("../assets/fonts/UASQUARE.ttf");
+pub const FONT_SQUARE: &[u8] = include_bytes!("../assets/fonts/UASQUARE.ttf");
 
-pub const FONT_NOVEMBER: &'static [u8] = include_bytes!("../assets/fonts/novem___.ttf");
+pub const FONT_NOVEMBER: &[u8] = include_bytes!("../assets/fonts/novem___.ttf");
 
-pub const FONT_KONGTEXT: &'static [u8] = include_bytes!("../assets/fonts/kongtext.ttf");
+pub const FONT_KONGTEXT: &[u8] = include_bytes!("../assets/fonts/kongtext.ttf");
 
-pub const LAUNCHER_BACKGROUND: &'static [u8] = include_bytes!("../assets/launcher/background.qoi");
+pub const LAUNCHER_BACKGROUND: &[u8] = include_bytes!("../assets/launcher/background.qoi");
