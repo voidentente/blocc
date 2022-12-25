4f05cd78f40261accf72268082d354d959ef7c5a
Voidentente <flood.void@gmail.com>
2022-12-25 18:48:46 +0100

    I forgor 💀

---
+++ client/profile_selection/src/lib.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+use client_state::GameState;
+
+pub struct ProfileSelectionPlugin;
+
+impl Plugin for ProfileSelectionPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(SystemSet::on_enter(GameState::Launcher).with_system(Profiles::update));
+        app.init_resource::<Profiles>();
+        app.init_resource::<ProfileSelection>();
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct Profiles(pub Vec<String>);
+
+impl Profiles {
+    fn update(mut commands: Commands) {
+        if let Ok(dir) = std::fs::read_dir("profiles") {
+            let mut vec = Vec::new();
+
+            for entry in dir.flatten() {
+                if entry.metadata().unwrap().is_dir() {
+                    vec.push(entry.file_name().into_string().unwrap());
+                }
+            }
+
+            commands.insert_resource(Self(vec));
+        }
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct ProfileSelection(pub Option<usize>);
