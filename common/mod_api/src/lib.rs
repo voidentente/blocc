601ea4323d25e3e1c73f5e0da3df3a3409a7f1de
Voidentente <flood.void@gmail.com>
2022-12-25 18:49:49 +0100

    Add base mod_api plugin

---
+++ common/mod_api/src/lib.rs
@@ -0,0 +1,88 @@
+//! A neutral sided mod loader that loads all `.bloc` files in the folder
+//! specified by the resource `ProfileSelection` in three `stages`,
+//! in a dynamically specified `order` by passing `&mut app`.
+
+use bevy::prelude::*;
+
+pub struct ModAPIPlugin;
+
+impl Plugin for ModAPIPlugin {
+    fn build(&self, _app: &mut App) {}
+}
+
+pub type Name = &'static str;
+
+pub type Version = (u8, u8, u8);
+
+pub type Authors = &'static [&'static str];
+
+pub type Dependencies = &'static [(Name, Version, Version)];
+
+pub type LoadBefore = &'static [Name];
+
+pub type LoadAfter = &'static [Name];
+
+/// Implement this trait on a struct to create a Blocc mod interface.
+/// Example:
+/// ```
+/// pub(crate) struct MyMod;
+///
+/// impl BloccMod for MyMod {
+///     ...
+/// }
+/// ```
+pub trait BloccMod {
+    /// The name of this mod.
+    /// Example:
+    /// ```
+    /// const NAME: Name = "Some Blocc Mod";
+    /// ```
+    const NAME: Name;
+
+    /// The version of this mod, formatted as SemVer.
+    /// Example:
+    /// ```
+    /// const VERSION: Version = (0, 1, 0);
+    /// ```
+    const VERSION: Version;
+
+    /// The author(s) of this mod.
+    /// Example:
+    /// ```
+    /// const AUTHORS: Authors = &["Author 1", "Author 2"];
+    /// ```
+    const AUTHORS: Authors;
+
+    /// The names of the mods and their minimum and maximum version requirement this mod depends on.
+    /// Example:
+    /// ```
+    /// const DEPENDENCIES: Dependencies = [
+    ///     ("World", (0, 1, 0), (0, 3, 4)),
+    ///     ("Some Blocc Mod", (0, 1, 0), (0, 1, 0)),
+    /// ]
+    /// ```
+    const DEPENDENCIES: Dependencies;
+
+    /// The names of the mods this mod should be loaded after.
+    /// Example:
+    /// ```
+    /// const LOAD_AFTER: LoadAfter = &["World"];
+    /// ```
+    const LOAD_AFTER: LoadAfter;
+
+    /// The names of the mods this mod should be loaded before.
+    /// Example:
+    /// ```
+    /// const LOAD_BEFORE: LoadBefore = &["Some Mod", "Other Mod"];
+    /// ```
+    const LOAD_BEFORE: LoadBefore;
+
+    /// Called in the first stage of mod loading by the modloader.
+    fn pre_init(app: &mut App);
+
+    /// Called in the second stage of mod loading by the modloader.
+    fn init(app: &mut App);
+
+    /// Called in the third and last stage of mod loading by the modloader.
+    fn post_init(app: &mut App);
+}
